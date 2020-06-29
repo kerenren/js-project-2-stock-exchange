9278b93046aef3a54138d1f3a1ab1e5a8cc158f7
Kelei <renkelei.kelly@gmail.com>
2020-06-29 23:43:07 +0300

    Mile Stone 8 done

--- js/SearchResult.js
+++ js/SearchResult.js
@@ -7,6 +7,7 @@ class SearchResult {
     this.corpAllInfoArr;
     this.inputEl = document.getElementById("search-query");
     this.query;
+    this.compareBtn;
     this.inputEl.addEventListener("input", () => {
       this.query = this.inputEl.value;
     });
@@ -47,6 +48,20 @@ class SearchResult {
     }
   }
 
+  createCompareEL(company, appendElement) {
+    this.compareBtn = document.createElement("button");
+    this.compareBtn.innerText = "Compare";
+    this.compareBtn.classList.add(
+      "float-right",
+      "btn",
+      "btn-success",
+      "compare"
+    );
+    this.compareBtn.setAttribute("justify-self", "end");
+    appendElement.append(this.compareBtn);
+    this.compareBtn.addEventListener("click", () => console.log(company));
+  }
+
   createResultElement(company) {
     const { name, symbol } = company;
     const { changes, image } = company.companiesProfileData[0]; // Object Destructuring
@@ -54,25 +69,33 @@ class SearchResult {
     const corpName = document.createElement("a");
     const corpIcon = document.createElement("img");
     const changeEl = document.createElement("span");
+    const corpContainer = document.createElement("div");
+    const symbolEL = document.createElement("div");
     const corpLink = `./company.html?symbol=${symbol}`;
     corpIcon.src = image;
     corpIcon.width = "35";
     corpIcon.classList.add("border-0");
     corpName.classList.add("border-0", "mx-3", "d-flex");
     changeEl.classList.add("border-0", "my-0", "ml-2");
+    corpContainer.classList.add("d-flex", "flex-grow-1");
     this.corpList.classList.add(
       "list-group-item",
       "d-flex",
-      "align-items-center"
+      "align-items-center",
+      "d-flex",
+      "justify-content-between"
     );
     corpName.innerHTML = this.highlightSearch(this.query, `${name}`);
     corpName.href = corpLink;
-    this.corpList.innerHTML = this.highlightSearch(this.query, ` (${symbol})`);
+    symbolEL.innerHTML = this.highlightSearch(this.query, ` (${symbol})`);
     changeEl.innerText = `(${changes}%)`;
     this.handleNumberColor(changes, changeEl);
-    this.corpList.append(changeEl);
-    this.corpList.prepend(corpName);
+    corpContainer.append(symbolEL);
+    corpContainer.append(changeEl);
+    corpContainer.prepend(corpName);
     this.corpList.prepend(corpIcon);
+    this.corpList.append(corpContainer);
+    this.createCompareEL(company, this.corpList);
     const resultFragment = new DocumentFragment();
     resultFragment.append(this.corpList);
     this.resultsUl.append(resultFragment);
