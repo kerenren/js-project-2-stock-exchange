dc45e352026f88f69197a4aedb1149ca1c311f7a
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-03 12:38:42 +0200

    updating the domain

--- public/js/api.js
+++ public/js/api.js
@@ -1,5 +1,5 @@
 export async function searchInInternalServal(search) {
-  const response = await fetch(`https://fierce-savannah-58662.herokuapp.com/api/search?query=${search}`);
+  const response = await fetch(`https://stock-exchange-js.herokuapp.com//api/search?query=${search}`);
   const data = await response.json();
   return data;
 }
